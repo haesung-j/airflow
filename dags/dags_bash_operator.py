c921ccf122787c277837850f1974649f5e5f5a85
haesung.sean <haesung.sean@gmail.com>
2024-01-31 23:53:13 +0900

    fix : dags_bash_operator.py
    각 TASK(bash_task1, bash_task)내 log 출력되지 않는 문제 해결

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -7,7 +7,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id='dags_bash_operator',  # 화면 표시 이름: 주로 파일명과 일치시킴
     schedule="0 0 * * *",  # cron 스케줄 (분 시 일 월 요일)
-    start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),  # 시작 날짜
+    start_date=pendulum.datetime(2022, 3, 1, tz='Asia/Seoul'),  # 시작 날짜
     catchup=False,  # (현재 3월 1일일 때, start_date가 1월 1일부터라면, True인 경우 누락 구간도 돌림(1/1~2/28)
     # dagrun_timeout=datetime.timedelta(minutes=60),  # 이 DAG이 60분이상 돌면 실패
     tags=['tag', 'tag2'],  # UI상 tag 표기
